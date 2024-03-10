3ba86dbf1c0a4b36ac1df190aec3a14c3104ee6f
Viktar Makayed <vmakoed@gmail.com>
2024-03-10 13:10:41 +0100

    Restrict allowed emails and domains

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :contestants, dependent: :destroy
   has_many :contests, through: :contestants
 
+  validates :email, :name, :provider, :uid, presence: true
   validates :score, numericality: { greater_than_or_equal_to: 0 }
+  validate :email_allowed, if: -> { email.present? }
 
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
@@ -13,7 +15,17 @@ class User < ApplicationRecord
     ).tap { |user|
       user.email = auth.info.email
       user.name = auth.info.name
-      user.save!
     }
+  end
+
+  private
+
+  def email_allowed
+    domain = email.split('@').last.downcase
+
+    return if ENV.fetch('ALLOWED_DOMAINS', '').split(',').include?(domain)
+    return if ENV.fetch('ALLOWED_EMAILS', '').split(',').include?(email.downcase)
+
+    errors.add(:email, 'is not allowed')
   end
 end
